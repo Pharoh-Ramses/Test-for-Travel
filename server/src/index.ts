19a2287c2c82d002ab83b17cbc47165e4a9ac38b
Ramses Angles <ramsescangles@gmail.com>
2022-10-05 16:20:08 -0500

    Finished loading Apollo Server and GraphQL into the environment.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,18 @@
 import express from 'express';
 import {ApolloServer} from 'apollo-server-express'; 
-import {schema} from './graphql'
+import {schema} from './graphql';
+
+
+
 const app = express();
 const port = 9000;
 
+
+
 const server = new ApolloServer({schema});
+//need to add the server.start function for Apollo 3
+server.start().then(_res => {
 server.applyMiddleware({app, path: '/api'});
 
-
-
-
- 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
-
- 
+});
