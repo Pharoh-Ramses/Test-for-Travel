1b5842c9cdc58dd9fd066b8ed1f9f5f29f6df652
Ramses Angles <ramsescangles@gmail.com>
2022-10-04 21:01:52 -0500

    Installed Apollo Server and have begun the GraphQL Schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,14 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import {locations} from "./locations"
+import {ApolloServer} from 'apollo-server-express'; 
+import {schema} from './graphql'
 const app = express();
 const port = 9000;
 
-app.use(bodyParser.json({limit: '50mb'}));
+const server = new ApolloServer({schema});
+server.applyMiddleware({app, path: '/api'});
 
 
-//get locations
-app.get("/locations", (_req, res) => {
-    return res.send(locations);
-});
-//delete-locations
- app.post("/delete-location", (req, res) => { 
-    const id:string = req.body.id;
 
-    for (let i = 0; i < locations.length; i++) {
-        if (locations[i].id === id) {
-            return res.send(locations.splice(i, 1));
-        }
-    }
-    return res.send("failed to delete locations");
- });
 
  
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
