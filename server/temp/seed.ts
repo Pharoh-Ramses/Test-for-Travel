e9e993edad9b8d4b462454dd27ddc9691e135d88
Ramses Angles <ramsescangles@gmail.com>
2022-11-18 16:26:46 -0600

    building seed script

---
+++ server/temp/seed.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+require("dotenv").config();
+import { connectDatabase } from "../src/database";
+
+const seed = async () => {
+  try {
+    console.log("[seed] : running...");
+    const db = await connectDatabase();
+  } catch (error) {
+    throw new Error("failed to seed database");
+  }
+};
+
+seed();
