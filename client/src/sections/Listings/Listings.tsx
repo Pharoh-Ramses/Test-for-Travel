1e7c7652d4d00bf51dc198582e2e309871521407
Ramses Angles <ramsesangles@pop-os.localdomain>
2023-01-27 11:59:14 -0600

    added the delete listing mutiation with type defined variables

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {server} from '../../lib/api'
+import {ListingsData, DeleteListingData, DeleteListingVariables} from './types'
  
 const LISTINGS = `
   query Listings{
@@ -17,6 +18,14 @@ const LISTINGS = `
   }
 `;
 
+const DELETE_LISTING = `
+mutation DeleteListing($id: ID!){
+  deleteListing(id: $id){
+    id
+  }
+}
+
+`;
 
 interface Props {
   title: string
@@ -25,16 +34,25 @@ interface Props {
 export const Listings = ({title}:Props) => {
   const fetchListings = async () => {
     try {
-      const {data} = await server.fetch({query: LISTINGS});
+      const {data} = await server.fetch<ListingsData>({query: LISTINGS});
       console.log(data);
     } catch (error) {
       throw new Error('Failed to fetch listings');
+    }
+  };
+  const deleteListing = async () => {
+    try {
+      const {data} = await server.fetch<DeleteListingData, DeleteListingVariables>({query: DELETE_LISTING, variables: {id: '637d4ab8cc0ef32e63e32b2f'}});
+      console.log(data);
+    } catch (error) {
+      throw new Error('Failed to delete listing');
     }
   };
   return (
     <div>
     <h2>{title}</h2>
     <button onClick={fetchListings}>Query Listings!</button>
+    <button onClick={deleteListing}>Delete a Listing!</button>
     </div>
   )
 };
